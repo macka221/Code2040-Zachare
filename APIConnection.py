78d065b9433ebc03e1703be84f4b8bb8b04347e1
macka221 <zachare18@gmail.com>
2016-10-31 18:48:51 -0400

    Add files via upload

---
+++ APIConnection.py
@@ -0,0 +1,10 @@
+import requests as request
+
+tokenVal = "d2f2bccb39ae22ad239c31197251640f"
+githubVal = "https://github.com/macka221/Code2040-Zachare"
+
+body = {"token":tokenVal, "github":githubVal}
+url ="http://challenge.code2040.org/api/register"
+API = request.post(url,json=body)
+print(API.content)
+input()
---
+++ APIQuestion1.py
@@ -0,0 +1,24 @@
+import requests as request
+
+endpoint = "http://challenge.code2040.org/api/reverse"
+validateUrl = "http://challenge.code2040.org/api/reverse/validate"
+tokenVal = "d2f2bccb39ae22ad239c31197251640f"
+stringVal = ""
+
+recieve_string = {"token": tokenVal, "string":""}
+
+inAPI = request.post(endpoint, json=recieve_string)
+temp = str(inAPI.content)
+stop = len(temp) - 1
+testAPI = temp[2:stop]
+
+i = 1
+while i <= len(testAPI):
+    stringVal = stringVal + testAPI[-i]
+    i += 1
+
+outString = {"token": tokenVal, "string":stringVal}
+outAPI = request.post(validateUrl, json=outString)
+
+print(outAPI.content)
+input()
