4518e2213cf684eec9dc9fc62c2eed7993423f76
macka221 <zachare18@gmail.com>
2016-11-01 19:33:39 -0400

    Add files via upload

---
+++ APIQuestion2.py
@@ -0,0 +1,29 @@
+import requests as request
+import json
+
+
+tokenVal = "d2f2bccb39ae22ad239c31197251640f"
+needle = ""
+haystack = []
+haystackURL = "http://challenge.code2040.org/api/haystack"
+validateURL = "http://challenge.code2040.org/api/haystack/validate"
+inDict = {"token":tokenVal, "needle":needle, "haystack":haystack}
+
+inAPI = request.post(haystackURL, json=inDict)
+tempStr = str(inAPI.content)
+
+tempStr = tempStr[2:-1]
+newDict = json.loads(tempStr)
+DictValues = newDict["haystack"]
+sentinal = True
+j = 0
+
+
+while sentinal:
+    if DictValues[j] == newDict['needle']:
+        sentinal = False
+    j+=1
+
+outDict = {"token":tokenVal, "needle":j - 1}
+outAPI = request.post(validateURL, json=outDict)
+print(outAPI.content)
---
+++ APIQuestion3.py
@@ -0,0 +1,32 @@
+import requests as request
+from json import loads as load
+
+
+tokenVal = "d2f2bccb39ae22ad239c31197251640f"
+prefixURL = "http://challenge.code2040.org/api/prefix"
+validateURL = "http://challenge.code2040.org/api/prefix/validate"
+prefix = ""
+array = []
+inDict = {"token":tokenVal, "prefix":prefix, "array":array}
+
+inAPI = request.post(prefixURL, json=inDict)
+
+tempStr = str(inAPI.content)
+tempStr = tempStr[2:-1]
+newDict = load(tempStr)
+DictArray = newDict['array']
+newPrefix = newDict['prefix']
+newArray = []
+prefixSize = len(newPrefix)
+sentinal = True
+
+for i in DictArray:
+    if i[:prefixSize] != newPrefix:
+        newArray.append(i)
+
+
+outDict = {'token':tokenVal, 'array':newArray}
+
+outAPI = request.post(validateURL, json=outDict)
+
+print(outAPI.content)
