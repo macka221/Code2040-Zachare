6f9b2391db9bdc7d4b725f8ae9ac6a5ba46bbd10
macka221 <zachare18@gmail.com>
2016-11-09 18:39:49 -0500

    Making a function

--- APIConnection.py
+++ APIConnection.py
@@ -3,9 +3,10 @@ import requests as request
 
 tokenVal = "d2f2bccb39ae22ad239c31197251640f"
 githubVal = "https://github.com/macka221/Code2040-Zachare"
+def requestAPI():
+  body = {"token":tokenVal, "github":githubVal}
+  url ="http://challenge.code2040.org/api/register"
+  API = request.post(url,json=body)
+  return API.content
 
-body = {"token":tokenVal, "github":githubVal}
-url ="http://challenge.code2040.org/api/register"
-API = request.post(url,json=body)
-print(API.content)
-input()
+print(requestAPI())
