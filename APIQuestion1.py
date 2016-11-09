7dcafbd287a3270d5475a90cfbf2ebb397528ed2
macka221 <zachare18@gmail.com>
2016-11-09 18:43:36 -0500

    Creating Functions

--- APIQuestion1.py
+++ APIQuestion1.py
@@ -6,20 +6,24 @@ validateUrl = "http://challenge.code2040.org/api/reverse/validate"
 tokenVal = "d2f2bccb39ae22ad239c31197251640f"
 stringVal = ""
 
-recieve_string = {"token": tokenVal, "string":""}
+def gatherString():
+    recieve_string = {"token": tokenVal, "string":""}
 
-inAPI = request.post(endpoint, json=recieve_string)
-temp = str(inAPI.content)
-stop = len(temp) - 1
-testAPI = temp[2:stop]
+    inAPI = request.post(endpoint, json=recieve_string)
+    temp = str(inAPI.content)
+    stop = len(temp) - 1
+    testAPI = temp[2:stop]
+    return testAPI
 
-i = 1
-while i <= len(testAPI):
-    stringVal = stringVal + testAPI[-i]
-    i += 1
+def reverseString(string)
+    i = 1
+    while i <= len(string):
+        stringVal = stringVal + string[-i]
+        i += 1
 
-outString = {"token": tokenVal, "string":stringVal}
-outAPI = request.post(validateUrl, json=outString)
+    outString = {"token": tokenVal, "string":stringVal}
+    outAPI = request.post(validateUrl, json=outString)
+    return outAPI.content
 
-print(outAPI.content)
-input()
+data = gatherString()
+print(reverseString(data))
