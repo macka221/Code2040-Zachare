3d9624e29fc931afb444601a7804e408cfe98017
macka221 <zachare18@gmail.com>
2016-11-09 18:50:12 -0500

    Adding Functions

--- APIQuestion2.py
+++ APIQuestion2.py
@@ -1,4 +1,4 @@
-#TODO: Add Comments and structurize
+#TODO: Add Comments
 import requests as request
 import json
 
@@ -6,25 +6,32 @@ import json
 tokenVal = "d2f2bccb39ae22ad239c31197251640f"
 needle = ""
 haystack = []
+
 haystackURL = "http://challenge.code2040.org/api/haystack"
 validateURL = "http://challenge.code2040.org/api/haystack/validate"
 inDict = {"token":tokenVal, "needle":needle, "haystack":haystack}
 
-inAPI = request.post(haystackURL, json=inDict)
-tempStr = str(inAPI.content)
+def getData():
+    inAPI = request.post(haystackURL, json=inDict)
+    tempStr = str(inAPI.content)
 
-tempStr = tempStr[2:-1]
-newDict = json.loads(tempStr)
-DictValues = newDict["haystack"]
-sentinal = True
-j = 0
+    tempStr = tempStr[2:-1]
+    newDict = json.loads(tempStr)
+    return newDict
+    
+def findNeedle(dict1):
+    DictValues = newDict["haystack"]
+    sentinal = True
+    j = 0
+    while sentinal:
+        if DictValues[j] == newDict['needle']:
+            sentinal = False
+        j+=1
+        
+    outDict = {"token":tokenVal, "needle":j - 1}
+    outAPI = request.post(validateURL, json=outDict)
+    return outAPI.content
 
 
-while sentinal:
-    if DictValues[j] == newDict['needle']:
-        sentinal = False
-    j+=1
-
-outDict = {"token":tokenVal, "needle":j - 1}
-outAPI = request.post(validateURL, json=outDict)
-print(outAPI.content)
+dict1 = getData()
+print(findNeedle(dict1))
